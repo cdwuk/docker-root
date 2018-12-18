b6b9452b58da5e398488c11c85297384e2246fc1
Nigel Wardle <n.wardle@uk.cdw.com>
2018-12-18 14:43:42 +0000

    ..

--- docker-apps/python-standard/app.py
+++ docker-apps/python-standard/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<div style='font-size:30pt;background-color:lightyellow'>Hello from a Python Standard<div>"
+    return "<h1>Hello from a Python Standard</h1><img src='./images/cdw.png' /><img src='./images/python.jpg' />"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

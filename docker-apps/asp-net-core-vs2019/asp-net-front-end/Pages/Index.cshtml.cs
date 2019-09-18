e9650e9641f36d517021859038fcebacaf48d8a1
Nigel Wardle <n.wardle@uk.cdw.com>
2019-09-18 20:55:09 +0100

    ..

---
+++ docker-apps/asp-net-core-vs2019/asp-net-front-end/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_net_front_end.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

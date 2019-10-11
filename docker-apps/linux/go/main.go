c33d19533714fe4a58f8e4f0ee8593d0d77ab4da
CDW UK <45399632+cdwuk@users.noreply.github.com>
2019-10-11 15:25:49 +0100

    Update main.go

--- docker-apps/linux/go/main.go
+++ docker-apps/linux/go/main.go
@@ -18,7 +18,7 @@ func main() {
 	port := getEnv("PORT", "8080")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "This is a Go executable listening on port 8080 returning this text.")
+		fmt.Fprintf(w, "This is a Go executable listening on port 8080, exposed on container port 8080, listening on a host port you have set.")
 	})
 
 	http.ListenAndServe(":"+port, nil)
